37d19abe777f4e97559065b2978e7aa355a07b73
Damir <damir.tulemisov@yandex.ru>
2022-05-17 17:34:36 +0500

    первое задание

--- My first website/script.js
+++ My first website/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
-var newVar = 12.4;
-let varString = "Advertisement";
-let number1 = 13,
-    number2 = 10;
-
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let answerOne = prompt("Введите обязательную статью расходов в этом месяце(товары,что нужно купить точно)");
+let answerTwo = +prompt("Во сколько обойдется?");
+let appData = {
+    userMoney: money,
+    timeData: time,
+    expenses: {
+        anwser: answerOne +":"+ answerTwo
+    },
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+alert("Ваш бюджет на один день:" + answerTwo/30 + " рублей");
